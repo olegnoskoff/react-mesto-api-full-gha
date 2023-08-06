ebb308c72de244d57874740a60fb6918df57deb9
Oleg <olegnoskoff@yandex.ru>
2023-08-06 11:37:42 +0300

    new fix

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,10 +1,10 @@
-const token = "d44b9fca-9567-456c-ae11-66fa146c6bcd";
-const cohortId = "cohort-64";
+// const token = "d44b9fca-9567-456c-ae11-66fa146c6bcd";
+// const cohortId = "cohort-64";
 
 export const apiConfig = {
-  baseUrl: `https://api.mesto2023.students.nomoreparties.co/v1/${cohortId}`,
+  baseUrl: `https://api.mesto2023.students.nomoreparties.co`,
   headers: {
-    authorization: token,
+    // authorization: token,
     "Content-Type": "application/json",
   },
 };
